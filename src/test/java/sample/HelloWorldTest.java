03605e566a0aca059898988100b45d3449e89a92
Chris Kimpton <chris@kimptoc.net>
2017-08-15 08:18:52 +0100

    Update HelloWorldTest.java

--- src/test/java/sample/HelloWorldTest.java
+++ src/test/java/sample/HelloWorldTest.java
@@ -1,7 +1,15 @@
 package sample;
 
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
 class HelloWorldTest {
 
   // TODO
-
+  @Test
+  public void tryHelloWorld(){
+    HelloWorld hw = new HelloWorld();
+    String result = hw.go();
+    assertEquals("foo",result);     
+  }
 }
