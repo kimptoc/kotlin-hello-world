c329fb7bf5a809ab670e1ec218fbe82203e594bc
Chris Kimpton <chris@kimptoc.net>
2017-08-15 08:42:58 +0100

    Update HelloWorldTest.java

--- src/test/java/sample/HelloWorldTest.java
+++ src/test/java/sample/HelloWorldTest.java
@@ -1,7 +1,9 @@
 package sample;
 
-import org.testng.annotations.Test;
-import static org.testng.Assert.*;
+//import org.testng.annotations.Test;
+//import static org.testng.Assert.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 class HelloWorldTest {
 
